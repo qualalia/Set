9a4b069d21fca4130ae59b4202dcd3ae47a0abf4
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-14 17:09:36 -0600

    add numberToTuple function

--- client/gameUtils.js
+++ client/gameUtils.js
@@ -1,8 +1,33 @@
+export const numberToTuple = x => {
+  if (x >= 81) console.log('out of range...somehow')
+  const tuple = [
+    x % 3,
+    Math.floor(x / 3) % 3,
+    Math.floor(x / 9) % 3,
+    Math.floor(x / 27)
+  ]
+  return tuple
+}
+
+export const checkSet = threeCards => {
+  if (threeCards.length < 3 || threeCards.length > 3)
+    return new Error('not enough cards')
+  for (let i = 0; i < 4; i++) {
+    let sum = 0
+    for (let j = 0; j < 4; j++) {
+      sum += threeCards[j][i]
+      console.log(threeCards[j][i])
+    }
+    console.log(sum)
+    if (sum % 3) return false
+  }
+  return true
+}
+
 export const dealCard = (deck, nextCardPos) => {
   if (nextCardPos >= 0 && nextCardPos < deck.length) {
     let toDeal = deck[nextCardPos]
-    toDeal.setFaceUp(true)
-    nextCardPos++
+    //    toDeal.setFaceUp(true)
     return toDeal
   }
   return 'No cards left!'
