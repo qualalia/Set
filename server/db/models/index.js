af6f2a2d353bbaa9bfa69b60f9d6a5fae772994d
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-13 21:24:49 -0600

    import models and add n:m associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,10 @@
-const User = require('./user')
+const User = require('./User')
+const Game = require('./Game')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+User.belongsToMany(Game, {through: 'GamePlayers'})
+Game.belongsToMany(User, {through: 'GamePlayers'})
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
 module.exports = {
-  User
+  User,
+  Game
 }
