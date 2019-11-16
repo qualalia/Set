106710268dff7955ae4773f2edb0c65f5f37fcad
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-16 17:03:29 -0600

    add /game router

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ module.exports = router
 
 router.use('/users', require('./users'))
 router.use('/deck', require('./deck.js'))
+router.use('/games', require('./game.js'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
