49b5e7cb4660652f1ce8cef27288fdad14d254d6
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-17 15:05:39 -0600

    add border on hover and on click

--- client/components/Board.js
+++ client/components/Board.js
@@ -5,10 +5,14 @@ import {Card} from '../components'
 
 const Board = props => {
   const game = useSelector(state => state.game)
+  const cardsOnTheBoard = game.cardsOnTheBoard
+
   const cardPlaces = []
   for (let i = 0; i < 12; i++) cardPlaces.push(i)
-  const cardsOnTheBoard = game.cardsOnTheBoard
+
+  const clickedCards = useSelector(state => state.setClickedCards)
   const dispatch = useDispatch()
+
   useEffect(() => {
     dispatch(newAnon())
   }, [])
@@ -16,7 +20,19 @@ const Board = props => {
   return cardsOnTheBoard ? (
     <div id="game-board">
       <div id="cards">
-        {cardPlaces.map(place => <Card which={cardsOnTheBoard[place]} />)}
+        {cardPlaces.map(place => (
+          <div
+            className="card-outer-div"
+            style={
+              clickedCards.includes(cardsOnTheBoard[place])
+                ? {border: 5 + 'px solid white', borderRadius: 25 + 'px'}
+                : {}
+            }
+            tabIndex={0}
+          >
+            <Card key={`c${place}`} which={cardsOnTheBoard[place]} />
+          </div>
+        ))}
       </div>
       <div className="your-sets">
         <div>10</div>
