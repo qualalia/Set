68b70afc865edf948f13b9e216a3ad513bd5809c
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-02-21 16:02:48 -0600

    minimal styling game over

--- client/components/EndGame.js
+++ client/components/EndGame.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const EndGame = props => {
   const {code} = props
-  return code ? <h1>GAME OVER</h1> : <h1>error</h1>
+  return code ? (
+    <h1 id="gg" style={{color: 'white'}}>
+      GAME OVER
+    </h1>
+  ) : (
+    <h1 style={{color: 'white'}}>error. how did you get here?</h1>
+  )
 }
 
 export default EndGame
