d2ab0c50c642826a69d7cd91fef2eed3fcc75313
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-22 12:07:06 -0500

    / routes to <Game />

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {withRouter, Route, Switch} from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { withRouter, Route, Switch } from "react-router-dom";
 import {
   Homescreen,
   Login,
@@ -8,23 +8,23 @@ import {
   UserHome,
   Game,
   EndGame,
-  Join
-} from './components'
-import {me} from './store'
+  Join,
+} from "./components";
+import { me } from "./store";
 //import {join} from './store/players.js'
 
 const Routes = () => {
-  const user = useSelector(state => state.user)
-  const isLoggedIn = !!user.email
-  const player = user
-  const dispatch = useDispatch()
+  const user = useSelector(state => state.user);
+  const isLoggedIn = !!user.email;
+  const player = user;
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(me)
-  }, [])
+    dispatch(me);
+  }, []);
   return (
     <Switch>
       {/* Routes placed here are available to all visitors */}
-      <Route exact path="/" render={() => <Homescreen />} />
+      <Route exact path="/" render={() => <Game />} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
       {isLoggedIn && (
@@ -44,7 +44,7 @@ const Routes = () => {
       />
       <Route path="/join" render={() => <Join userId={user.id} />} />
     </Switch>
-  )
-}
+  );
+};
 
-export default withRouter(Routes)
+export default withRouter(Routes);
