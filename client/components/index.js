17e40cfb94ffabf4728e70d74e2366912cc86b4a
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-23 16:58:38 -0500

    adds RulesModal

--- client/components/index.js
+++ client/components/index.js
@@ -3,14 +3,15 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Navbar} from './NavBar.js'
-export {default as UserHome} from './UserHome.js'
-export {Login, Signup} from './AuthForm'
-export {default as Card} from './Card.js'
-export {default as Board} from './Board.js'
-export {default as Game} from './Game'
-export {default as DealPile} from './DealPile'
-export {default as EndGame} from './EndGame.js'
-export {default as Join} from './Join.js'
-export {default as App} from '../app.js'
-export {default as Homescreen} from './Homescreen.js'
+export { default as Navbar } from "./NavBar.js";
+export { default as UserHome } from "./UserHome.js";
+export { Login, Signup } from "./AuthForm";
+export { default as Card } from "./Card.js";
+export { default as Board } from "./Board.js";
+export { default as Game } from "./Game";
+export { default as DealPile } from "./DealPile";
+export { default as EndGame } from "./EndGame.js";
+export { default as Join } from "./Join.js";
+export { default as App } from "../app.js";
+export { default as Homescreen } from "./Homescreen.js";
+export { default as RulesModal } from "./RulesModal.js";
