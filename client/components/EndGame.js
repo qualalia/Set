0c398515647915482ecef45bdb8bc7978e5dd844
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-23 16:55:58 -0500

    bulks out Game Over screen by adding sets you got and New Game button

--- client/components/EndGame.js
+++ client/components/EndGame.js
@@ -1,14 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Header, Button } from "semantic-ui-react";
 
 const EndGame = props => {
-  const {code} = props
+  const { code, sets } = props;
+  const history = useHistory();
   return code ? (
-    <h1 id="gg" style={{color: 'white'}}>
-      GAME OVER
-    </h1>
+    <div className="game-board gg">
+      <Header
+        inverted
+        as="h1"
+        size="huge"
+        textAlign="center"
+        content="GAME OVER"
+      />
+      <Header
+        className="gg"
+        inverted
+        as="h2"
+        textAlign="center"
+        content={`You got ${sets === 1 ? sets + " set." : sets + " sets."}`}
+      />
+      <Header textAlign="center">
+        <Button
+          color="violet"
+          size="huge"
+          onClick={() => history.push("/solo")}
+        >
+          <i className="plus icon" />
+          New Game
+        </Button>
+      </Header>
+    </div>
   ) : (
-    <h1 style={{color: 'white'}}>error. how did you get here?</h1>
-  )
-}
+    <Header inverted as="h1" content="error. how did you get here?" />
+  );
+};
 
-export default EndGame
+export default EndGame;
+
+/*style={{color: 'white'}}>
+   GAME OVER
+   </h1>
+
+   <h1 style={{color: 'white'}}>error. how did you get here?</h1>
+ */
