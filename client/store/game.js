0ec75b9663bf9859df2c68d6154dc707e35255cb
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-23 10:48:52 -0500

    makes update game PUT route more RESTful

--- client/store/game.js
+++ client/store/game.js
@@ -1,57 +1,57 @@
-import history from '../history'
-const axios = require('axios')
-const {red} = require('chalk')
+import history from "../history";
+const axios = require("axios");
+const { red } = require("chalk");
 
-const SET_GAME = 'SET_GAME'
-const initialState = {}
-const setGame = game => ({type: SET_GAME, game})
+const SET_GAME = "SET_GAME";
+const initialState = {};
+const setGame = game => ({ type: SET_GAME, game });
 
 export const newGame = () => async dispatch => {
   try {
-    const {data} = await axios.post('/api/games/new')
-    dispatch(setGame(data))
-    history.push(`/play/${data.code}`)
+    const { data } = await axios.post("/api/games/new");
+    dispatch(setGame(data));
+    history.push(`/play/${data.code}`);
   } catch (err) {
-    console.error(red(err))
+    console.error(red(err));
   }
-}
+};
 
 export const getGame = code => async dispatch => {
   try {
-    const {data} = await axios.get(`/api/games/${code}`)
-    dispatch(setGame(data))
-    history.push(`/play/${code}`)
+    const { data } = await axios.get(`/api/games/${code}`);
+    dispatch(setGame(data));
+    history.push(`/play/${code}`);
   } catch (err) {
-    console.error(red(err))
+    console.error(red(err));
   }
-}
+};
 
 export const updateGame = (theSet, playerId, gameId) => async dispatch => {
   try {
-    const {data} = await axios.put(
-      `/api/games/${gameId}/${playerId}/update-board`,
-      theSet
-    )
-    dispatch(setGame(data))
+    const { data } = await axios.put(
+      `/api/games/${gameId}/player/${playerId}/update-board`,
+      theSet,
+    );
+    dispatch(setGame(data));
   } catch (err) {
-    console.error(red(err))
+    console.error(red(err));
   }
-}
+};
 
 export const stumped = gameId => async dispatch => {
   try {
-    const {data} = await axios.get(`/api/games/${gameId}/stumped`)
-    dispatch(setGame(data))
+    const { data } = await axios.get(`/api/games/${gameId}/stumped`);
+    dispatch(setGame(data));
   } catch (err) {
-    console.error(red(err))
+    console.error(red(err));
   }
-}
+};
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case SET_GAME:
-      return action.game
+      return action.game;
     default:
-      return state
+      return state;
   }
 }
--- server/api/game.js
+++ server/api/game.js
@@ -1,91 +1,89 @@
-const router = require('express').Router()
-const {Game, User, GamePlayer} = require('../db/models')
+const router = require("express").Router();
+const { Game, User, GamePlayer } = require("../db/models");
 const {
   checkSet,
   findSet,
   shuffle,
-  numberToTuple
-} = require('../../client/gameUtils.js')
-const customId = require('custom-id')
-const {io} = require('./index.js')
-module.exports = router
+  numberToTuple,
+} = require("../../client/gameUtils.js");
+const customId = require("custom-id");
+const { io } = require("./index.js");
+module.exports = router;
 
 //const CARDS_IN_DECK = 81
-const CARDS_IN_DECK = 81
+const CARDS_IN_DECK = 81;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const games = await Game.findAll()
-    res.json(games)
+    const games = await Game.findAll();
+    res.json(games);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.get('/:code', async (req, res, next) => {
+router.get("/:code", async (req, res, next) => {
   try {
     const game = await Game.findOne({
-      where: {code: req.params.code}
-    })
-    res.json(game)
+      where: { code: req.params.code },
+    });
+    res.json(game);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.post('/new', async (req, res, next) => {
+router.post("/new", async (req, res, next) => {
   try {
-    const deck = []
-    const firstTwelve = []
-    for (let i = 0; i < CARDS_IN_DECK; i++) deck.push(i)
-    shuffle(deck)
-    for (let i = 0; i < 12; i++) firstTwelve.push(deck[i])
+    const deck = [];
+    const firstTwelve = [];
+    for (let i = 0; i < CARDS_IN_DECK; i++) deck.push(i);
+    shuffle(deck);
+    for (let i = 0; i < 12; i++) firstTwelve.push(deck[i]);
     const newGame = await Game.create({
       code: customId({}),
       deck,
       cardsOnTheBoard: firstTwelve,
       nextCardPos: 12,
       cardsLeft: CARDS_IN_DECK - 12,
-      foundSet: findSet(firstTwelve)
-    })
-    res.status(200).json(newGame)
+      foundSet: findSet(firstTwelve),
+    });
+    res.status(200).json(newGame);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.put('/:gId/:pId/update-board', async (req, res, next) => {
+router.put("/:gId/player/:pId/update-board", async (req, res, next) => {
   try {
-    let threeCards = req.body
-    const tuples = threeCards.map(n => numberToTuple(n))
-    const isSet = checkSet(tuples)
-    const game = await Game.findByPk(+req.params.gId)
-    let {cardsOnTheBoard, nextCardPos, cardsLeft, foundSet} = game
-    const min = Math.min(3, cardsLeft)
+    let threeCards = req.body;
+    const tuples = threeCards.map(n => numberToTuple(n));
+    const isSet = checkSet(tuples);
+    const game = await Game.findByPk(+req.params.gId);
+    let { cardsOnTheBoard, nextCardPos, cardsLeft, foundSet } = game;
+    const min = Math.min(3, cardsLeft);
     if (isSet) {
       // clicked cards form a set
+      const sets = game.sets + 1;
       //      const player = await User.findByPk(+req.params.pId)
       if (cardsOnTheBoard.length > 12 || min === 0)
-        // had been stumped or no cards left --> don't deal three more
+        // had been stumped or no cards left, so don't deal three more
         for (let i = 0; i < 3; i++)
-          cardsOnTheBoard.splice(cardsOnTheBoard.indexOf(threeCards[i]), 1)
+          cardsOnTheBoard.splice(cardsOnTheBoard.indexOf(threeCards[i]), 1);
       else {
         // replace clicked cards with 3 new cards
         for (let i = 0; i < Math.min(3, min); i++)
           cardsOnTheBoard.splice(
             cardsOnTheBoard.indexOf(threeCards[i]),
             1,
-            game.deck[nextCardPos++]
-          )
+            game.deck[nextCardPos++],
+          );
       }
-      console.log(`current set stored on game: `, foundSet)
-      let i = 0
-      while (i < 2 && threeCards.includes(foundSet[i])) i++
+      let i = 0;
+      while (i < 2 && threeCards.includes(foundSet[i])) i++;
       if (i === 2) {
-        foundSet = findSet(cardsOnTheBoard)
-        console.log(`new stored set on game: ${foundSet}`)
+        foundSet = findSet(cardsOnTheBoard);
       }
-      console.log('was a different set: ', threeCards)
       // if they're the same, need new set to store in `foundSet`
 
       /*      const updatedPlayer = await User.update(
@@ -97,66 +95,68 @@ router.put('/:gId/:pId/update-board', async (req, res, next) => {
           cardsOnTheBoard,
           nextCardPos,
           cardsLeft: CARDS_IN_DECK - nextCardPos,
-          foundSet
+          foundSet,
+          sets,
         },
-        {where: {id: game.id}, returning: true, plain: true}
-      ))[1]
+        { where: { id: game.id }, returning: true, plain: true },
+      ))[1];
       if (cardsLeft === 0) {
-        if (foundSet.length > 0) res.status(201).json(updatedGame)
+        if (foundSet.length > 0) res.status(201).json(updatedGame);
         else {
-          // if none, game over
+          // if no cards left, it's game over
           updatedGame = (await Game.update(
             {
               cardsOnTheBoard,
               nextCardPos,
               cardsLeft: CARDS_IN_DECK - nextCardPos,
               gg: true,
-              foundSet: []
+              foundSet: [],
+              sets,
             },
-            {where: {id: game.id}, returning: true, plain: true}
-          ))[1]
-          console.log(`no sets left`)
-          res.status(202).send(updatedGame)
+            { where: { id: game.id }, returning: true, plain: true },
+          ))[1];
+          console.log(`no sets left`);
+          res.status(202).send(updatedGame);
         }
       } else {
         // there are still cards left
-        res.status(201).json(updatedGame)
+        res.status(201).json(updatedGame);
       }
-    } else res.status(200).send(game)
+    } else res.status(200).send(game);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.put('/:id/players', async (req, res, next) => {
+router.put("/:id/players", async (req, res, next) => {
   try {
     // TODO: add players
-    const userId = req.body
-    const playerToAdd = await User.findByPk(userId)
-    const gameId = parseInt(req.params.id, 10)
-    const game = await Game.findByPk(gameId)
+    const userId = req.body;
+    const playerToAdd = await User.findByPk(userId);
+    const gameId = parseInt(req.params.id, 10);
+    const game = await Game.findByPk(gameId);
     //    console.log(playerToAdd)
-    game.addPlayers(playerToAdd)
-    res.status.json(game)
+    game.addPlayers(playerToAdd);
+    res.status.json(game);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.get('/:gId/stumped', async (req, res, next) => {
+router.get("/:gId/stumped", async (req, res, next) => {
   try {
-    const game = await Game.findByPk(+req.params.gId)
-    let {cardsOnTheBoard, nextCardPos, deck} = game
-    const min = Math.min(81 - nextCardPos, 3)
+    const game = await Game.findByPk(+req.params.gId);
+    let { cardsOnTheBoard, nextCardPos, deck } = game;
+    const min = Math.min(81 - nextCardPos, 3);
     for (let i = 0; i < min; i++) {
-      cardsOnTheBoard.push(deck[nextCardPos++])
+      cardsOnTheBoard.push(deck[nextCardPos++]);
     }
     const updatedGame = await Game.update(
-      {cardsOnTheBoard, nextCardPos},
-      {where: {id: game.id}, returning: true, plain: true}
-    )
-    res.status(201).json(updatedGame[1])
+      { cardsOnTheBoard, nextCardPos },
+      { where: { id: game.id }, returning: true, plain: true },
+    );
+    res.status(201).json(updatedGame[1]);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
