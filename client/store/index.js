0a6736e489f25fc4a59b01ded3c68e39b0229140
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-04-29 11:08:23 -0500

    removes redux logger

--- client/store/index.js
+++ client/store/index.js
@@ -1,13 +1,13 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import user from './user'
-import game from './game.js'
-import setClickedCards from './toggleClicked'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import user from "./user";
+import game from "./game.js";
+import setClickedCards from "./toggleClicked";
 //import checkSet from './checkSet'
-import players from './players.js'
-import hint from './hint.js'
+import players from "./players.js";
+import hint from "./hint.js";
 
 const reducer = combineReducers({
   user,
@@ -15,16 +15,14 @@ const reducer = combineReducers({
   setClickedCards,
   game,
   hint,
-  players
-})
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
+  players,
+});
+const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware));
+const store = createStore(reducer, middleware);
 
-export default store
-export * from './user'
-export * from './game'
-export * from './hint'
-export * from './players'
-export * from './toggleClicked'
+export default store;
+export * from "./user";
+export * from "./game";
+export * from "./hint";
+export * from "./players";
+export * from "./toggleClicked";
